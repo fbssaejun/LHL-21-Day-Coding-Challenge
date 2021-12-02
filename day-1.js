38087debec7b27288838ee8a3a06fb7b93592ebd
Anthony Kim <fbssaejun@gmail.com>
2021-12-02 16:21:30 +0900

    create a function that will take two string parameters and return a string containing the sender name and the message

---
+++ day-1.js
@@ -0,0 +1,22 @@
+/* Instructions
+Click here to learn how to navigate the code editor
+
+Our latest testing indicates a malfunctioning component in our communications apparatus. For some reason, messages from mission control are not being correctly displayed on board the shuttle. While the message is being displayed on the shuttle’s terminal, the sender’s name is not displayed beside it.
+
+Your first task is to write a function that will take two string parameters and return a string containing the sender name and the message in the form specified in the instructions.
+
+Examples
+Input:
+
+    parseMessage("Mission Control", "Hello there!")
+    
+Output:
+
+    Mission Control: Hello there!
+*/
+
+const parseMessage = (origin, message) => {
+  return `${origin}: ${message}`
+}
+
+    
